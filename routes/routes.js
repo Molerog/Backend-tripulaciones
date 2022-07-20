b2459461317d65f7eee85d0259fb3c7e02bf3e4a
Molerog <102958319+Molerog@users.noreply.github.com>
2022-07-20 21:42:39 +0200

    probando

--- routes/routes.js
+++ routes/routes.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { authentication } = require('../middleware/authentication');
-const RouteController = require('../controllers/RouteController');
+const { authentication } = require("../middleware/authentication");
+const RouteController = require("../controllers/RouteController");
 
-router.post('/', RouteController.create);
-router.get('/', authentication, RouteController.getAll);
+router.post("/", RouteController.create);
+router.get("/", authentication, RouteController.getAll);
 router.get(
-  '/allRoutesPage',
+  "/allRoutesPage",
   authentication,
   RouteController.getAllRoutesPaginated
 );
-router.put('/likes/:_id', authentication, RouteController.like);
-router.get('/route/:_id', authentication, RouteController.getById);
-router.put('/dislike/:_id', authentication, RouteController.dislike);
+router.get("/route/:_id", authentication, RouteController.getById);
+router.put("/likes/:_id", authentication, RouteController.like);
+router.put("/dislike/:_id", authentication, RouteController.dislike);
 
 module.exports = router;
