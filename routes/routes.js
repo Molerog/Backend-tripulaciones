0e3461f370d74b5379d1d467225828cfc71ceeb6
Rebeca <rebeca.as1994@gmail.com>
2022-07-20 18:19:58 +0200

    routes getById implementado correctamente

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { authentication } = require('../middleware/authentication');
-
 const RouteController = require('../controllers/RouteController');
 
 router.post('/', RouteController.create);
 router.get('/', authentication, RouteController.getAll);
-router.get(
-  '/allRoutesPage',
-  authentication,
-  RouteController.getAllRoutesPaginated
-);
+router.get('/allRoutesPage', authentication, RouteController.getAllRoutesPaginated);
 router.put('/likes/:_id', authentication, RouteController.like);
+router.get('/route/:_id', authentication, RouteController.getById);
 
-module.exports = router;
+module.exports = router
