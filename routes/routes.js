e038e0084bc861193aa7ccd03e578ab6ff65e260
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-07-20 14:56:45 +0200

    autenticacion

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const {authentication} = require('../middleware/authentication');
+
 const RouteController = require ("../controllers/RouteController");
 
-router.get('/', RouteController.getAll);
+router.get('/',authentication, RouteController.getAll);
 
 
 module.exports = router;
