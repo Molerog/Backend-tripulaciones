89a35595d7b31e51a0a7024bae30be7ac7d7ab12
Vincorp21 <vibarcar@gmail.com>
2022-07-24 22:23:47 +0200

    ok

--- models/Comment.js
+++ models/Comment.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.SchemaTypes.ObjectId;
 
-const CommentSchema = new mongoose.Schema({
+const CommentSchema = new mongoose.Schema(
+  {
     body: {
       type: String,
       required: [true, 'Por favor, introduce un texto'],
@@ -9,15 +10,17 @@ const CommentSchema = new mongoose.Schema({
 
     imagepath: String,
     userId: {
-        type: ObjectId,
-        ref: 'User'
+      type: ObjectId,
+      ref: 'User',
     },
     routeId: {
-        type: ObjectId,
-        ref: 'Route'
-    }
-}, { timestamps: true });
+      type: ObjectId,
+      ref: 'Route',
+    },
+  },
+  { timestamps: true }
+);
 
 const Comment = mongoose.model('Comment', CommentSchema);
 
-module.exports = Comment
+module.exports = Comment;
